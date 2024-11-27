3fd2a256c3665447ede6c47eac904dbab98bc5d0
Thomaspk98 <118299903+Thomaspk98@users.noreply.github.com>
2024-11-28 01:05:17 +0530

    Added rust detection as a service.

--- handlers.py
+++ handlers.py
@@ -5,3 +5,7 @@ class Handlers:
     def get_api_version():
         version=wingeye_services.get_api_version()
         return version
+    
+    def rust_detection(image_path):
+        rust=wingeye_services.detect_rust_with_boxes(image_path)
+        return rust
--- routers.py
+++ routers.py
@@ -1,8 +1,12 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from handlers import Handlers as wingeye_handlers
+import os
 
+SAVE_FOLDER_PATH="Uploaded Image"
+if not os.path.exists(SAVE_FOLDER_PATH):
+            os.mkdir(SAVE_FOLDER_PATH)
 
 wingeye=FastAPI()
 wingeye.add_middleware(
@@ -20,6 +24,18 @@ def get_api_version():
     return version
 
 
+@wingeye.post("/rust_detection")
+async def rust_detection(input_file: list[UploadFile] | None = None):
+    if not input_file:
+        return {"message": "No upload file sent"}
+    else:
+        for file in input_file:
+            file_location = f"{SAVE_FOLDER_PATH}/{file.filename}"
+            with open(file_location, "wb+") as file_object:
+                file_object.write(file.file.read())
+            rest_detect=wingeye_handlers.rust_detection(file_location)
+            rest_detect
+        return
 
 if __name__ == "__main__":
     uvicorn.run(wingeye, host="localhost", port=2024)
